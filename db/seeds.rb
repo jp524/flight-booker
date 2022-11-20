fe2a3a4f48825d75717047a124eaae7eac7c85a6
JP <85654561+jp524@users.noreply.github.com>
2022-11-19 20:58:28 -0500

    Create Airport model and database seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Airport.create([{ code: 'ATL' }, { code: 'PEK' }, { code: 'LAX' }, { code: 'DXB' }, { code: 'HND' },
+                { code: 'ORD' }, { code: 'LHR' }, { code: 'PVG' }, { code: 'CDG' }, { code: 'DFW' }])
