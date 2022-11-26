a3d61340e5012964683887b32cf63efccd6177a1
JP <85654561+jp524@users.noreply.github.com>
2022-11-26 13:27:14 -0500

    Build search to find Flight based on departure and arrival airports

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -7,7 +7,25 @@ class FlightsController < ApplicationController
     @flights.each do |flight|
       @departure_airports << flight.departure_airport
       @arrival_airports << flight.arrival_airport
-      @departure_dates << flight.start_datetime.strftime("%b %d, %Y")
+      @departure_dates << format_date(flight.start_datetime)
     end
+    @search_results = search_flights(flight_params)
+  end
+
+  private
+
+  def format_date(datetime)
+    [datetime.strftime("%b %d, %Y"), datetime.beginning_of_day]
+  end
+
+  def flight_params
+    params.permit(:departure_airport, :arrival_airport, :date, :passengers, :commit)
+  end
+
+  def search_flights(flight_params)
+    # date = flight_params[:date]
+    @flights.where("departure_airport_id = ?", flight_params[:departure_airport])
+            .where("arrival_airport_id = ?", flight_params[:arrival_airport])
+            # .where(start_datetime: (date))
   end
 end
