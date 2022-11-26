90b298959b0d1f243ce467a8f921606310b9caa8
JP <85654561+jp524@users.noreply.github.com>
2022-11-26 18:15:12 -0500

    Finalize search for Flights with date param

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,19 +1,18 @@
 class FlightsController < ApplicationController
   def index
     @flights = Flight.all
-    # @search_results = search_flights(flight_params)
+    @search_results = search_flights(flight_params) if params
   end
 
   private
 
   def flight_params
-    params.permit(:departure_airport, :arrival_airport, :date, :passengers, :commit)
+    params.permit(:departure_airport_id, :arrival_airport_id, :departure_date, :passengers, :commit)
   end
 
   def search_flights(flight_params)
-    # date = flight_params[:date]
-    @flights.where("departure_airport_id = ?", flight_params[:departure_airport])
-            .where("arrival_airport_id = ?", flight_params[:arrival_airport])
-            # .where(start_datetime: (date))
+    Flight.where(departure_airport_id: flight_params[:departure_airport_id])
+          .where(arrival_airport_id: flight_params[:arrival_airport_id])
+          .where(departure_date: flight_params[:departure_date])
   end
 end
