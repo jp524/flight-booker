96fb3e7fdfb4a46b8465187c9437e51532667ff4
JP <85654561+jp524@users.noreply.github.com>
2022-11-26 17:47:36 -0500

    Refactor Flight model and view

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,22 +1,10 @@
 class FlightsController < ApplicationController
   def index
-    @flights = Flight.all.includes(:departure_airport, :arrival_airport)
-    @departure_airports = []
-    @arrival_airports = []
-    @departure_dates = []
-    @flights.each do |flight|
-      @departure_airports << flight.departure_airport
-      @arrival_airports << flight.arrival_airport
-      @departure_dates << format_date(flight.start_datetime)
-    end
-    @search_results = search_flights(flight_params)
+    @flights = Flight.all
+    # @search_results = search_flights(flight_params)
   end
 
   private
-
-  def format_date(datetime)
-    [datetime.strftime("%b %d, %Y"), datetime.beginning_of_day]
-  end
 
   def flight_params
     params.permit(:departure_airport, :arrival_airport, :date, :passengers, :commit)
--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,4 +1,10 @@
 class Flight < ApplicationRecord
-  belongs_to :departure_airport, class_name: "Airport"
-  belongs_to :arrival_airport, class_name: "Airport"
+  belongs_to :departure_airport, class_name: 'Airport'
+  belongs_to :arrival_airport, class_name: 'Airport'
+
+  scope :distinct_departure_dates, -> { select(:departure_date).distinct }
+
+  def departure_date_formatted
+    departure_date.strftime('%b %d, %Y')
+  end
 end
