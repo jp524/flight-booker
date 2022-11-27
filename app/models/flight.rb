72395518aa83987ebfbf729a58c8cf333c7cfb8d
JP <85654561+jp524@users.noreply.github.com>
2022-11-27 15:46:08 -0500

    Add Booking model

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,6 +1,7 @@
 class Flight < ApplicationRecord
   belongs_to :departure_airport, class_name: 'Airport'
   belongs_to :arrival_airport, class_name: 'Airport'
+  has_many :bookings
 
   scope :distinct_departure_dates, -> { select(:departure_date).distinct }
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_26_235002) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_27_204332) do
   create_table "airports", force: :cascade do |t|
     t.string "code"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "city"
+  end
+
+  create_table "bookings", force: :cascade do |t|
+    t.integer "flight_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["flight_id"], name: "index_bookings_on_flight_id"
   end
 
   create_table "flights", force: :cascade do |t|
@@ -31,6 +38,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_26_235002) do
     t.index ["departure_airport_id"], name: "index_flights_on_departure_airport_id"
   end
 
+  add_foreign_key "bookings", "flights"
   add_foreign_key "flights", "airports", column: "arrival_airport_id"
   add_foreign_key "flights", "airports", column: "departure_airport_id"
 end
