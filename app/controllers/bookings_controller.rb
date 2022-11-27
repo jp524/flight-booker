f9f509f3a4c7d5bafa308f5c9715e48fbdd3a1ba
JP <85654561+jp524@users.noreply.github.com>
2022-11-27 17:00:21 -0500

    Modify Booking#new form to allow for multiple passengers

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,6 +1,8 @@
 class BookingsController < ApplicationController
   def new
     @booking = Booking.new
+    params[:passengers].to_i.times { @booking.passengers.build }
+    @selected_flight = Flight.find(params[:flight_id])
   end
 
   def create
